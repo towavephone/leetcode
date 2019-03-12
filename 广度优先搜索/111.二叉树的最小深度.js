3b90477b6b912c2e65f1e03586af3842fccfb05b
towavephone <634407147@qq.com>
2019-03-12 12:10:12 +0800

    分门别类

--- 广度优先搜索/111.二叉树的最小深度.js
+++ 广度优先搜索/111.二叉树的最小深度.js
@@ -61,3 +61,10 @@ var minDepth = function(root) {
     }
 };
 
+// var minDepth = function(root) {
+//     if (!root) {
+//       return 0;
+//     }
+//     return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+// };
+
